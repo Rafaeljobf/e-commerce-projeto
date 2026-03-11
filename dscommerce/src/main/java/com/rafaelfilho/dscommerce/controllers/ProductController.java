2eba7ec39fe3604ca584d86b24027908560c5195
Rafael Oliveira <rafaf1604@gmail.com>
2026-03-10 23:53:24 -0300

    inicio de estruturação de modelo em camadas com endpoint GET product by id implementado

---
+++ dscommerce/src/main/java/com/rafaelfilho/dscommerce/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.rafaelfilho.dscommerce.controllers;
+
+import com.rafaelfilho.dscommerce.dto.ProductDTO;
+import com.rafaelfilho.dscommerce.entities.Product;
+import com.rafaelfilho.dscommerce.repositories.ProductRepository;
+import com.rafaelfilho.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+       return service.findById(id);
+    }
+}
