6d606eafa38732357ddfc09c5700bac338d6225f
Rafael Oliveira <rafaf1604@gmail.com>
2026-09-24 20:19:35 -0300

    fix validations

--- dscommerce/src/main/java/com/rafaelfilho/dscommerce/dto/ValidationError.java
+++ dscommerce/src/main/java/com/rafaelfilho/dscommerce/dto/ValidationError.java
@@ -17,6 +17,8 @@ public class ValidationError extends CustomError {
     }
 
     public void addError(String fieldName, String message){
+        errors.removeIf(e -> e.getFieldName().equals(fieldName));
+
         errors.add(new FieldMessage(fieldName, message));
     }
 }
